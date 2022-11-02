6f7a7d0ddb58dc74534193ca1c732bc1fb27dbf6
drichards188 <github@cxoprotect.com>
2022-11-02 10:15:00 -0600

    displaying poll based on id in url

--- src/features/polling/Poll.tsx
+++ src/features/polling/Poll.tsx
@@ -1,7 +1,31 @@
 import Header from "../misc/Header";
 import PollOption from "./PollOption";
+import {useSearchParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import {useAppSelector} from "../../app/hooks";
+import {selectPolls} from "./pollingSlice";
 
 const Poll = () => {
+    const polls = useAppSelector(selectPolls);
+
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [pollData, setPollData] = useState({
+        id: 0,
+        option1: 'option1',
+        option2: 'option2',
+        answered1: 0,
+        answered2: 0, name: 'name', time: '3:00 pm', date: '01/01/2022'
+    });
+
+    useEffect(() => {
+        let idParam = searchParams.get('id');
+        let desiredPoll: number;
+        if (typeof idParam === 'string') {
+            desiredPoll = parseInt(idParam);
+        }
+        let poll = polls.find((poll: any) => poll.id === desiredPoll);
+        setPollData(poll);
+    })
 
     const containerStyle = {
         backgroundColor: "#C9ced2",
@@ -16,8 +40,8 @@ const Poll = () => {
             <Header/>
             <h1>Would You Rather</h1>
             <div style={containerStyle}>
-                <PollOption pollData={{question: 'Build our new app with JS', id: 1}}/>
-                <PollOption pollData={{question: 'Build with TS', id:1}}/>
+                <PollOption pollData={{question: pollData.option1, id: 1}}/>
+                <PollOption pollData={{question: pollData.option2, id: 1}}/>
             </div>
         </div>
     )
