40d550714c7c58eb1ffb1540b698472dfabe5b94
drichards188 <github@cxoprotect.com>
2022-11-02 13:00:40 -0600

    creating polls works. need a way to generate ids

--- src/features/polling/CreatePoll.tsx
+++ src/features/polling/CreatePoll.tsx
@@ -1,7 +1,16 @@
 import {Button, TextField} from "@mui/material";
 import Header from "../misc/Header";
+import {useAppDispatch, useAppSelector} from "../../app/hooks";
+import {addPoll, selectUser} from "./pollingSlice";
+import {useState} from "react";
 
 const CreatePoll = () => {
+    const dispatch = useAppDispatch();
+    const user = useAppSelector(selectUser);
+
+
+    const [optionOne, setOptionOne] = useState('');
+    const [optionTwo, setOptionTwo] = useState('');
 
     const containerStyle = {
         backgroundColor: "#C9ced2",
@@ -11,15 +20,22 @@ const CreatePoll = () => {
         width: '80%'
     }
 
+    const createPoll = () => {
+
+        const poll = {option1: optionOne, option2: optionTwo, author: user.name, answered1: 0, answered2: 0, time: '3:00 pm', date: '11/22/2022'};
+        // alert(JSON.stringify(poll));
+        dispatch(addPoll({poll: poll}));
+    }
+
     return (
         <div>
-        <Header />
-        <div style={containerStyle}>
-            <h1>Create Your own poll</h1>
-            <TextField id="outlined-basic" label="Option One" variant="outlined" />
-            <TextField id="outlined-basic" label="Option Two" variant="outlined" />
-            <Button variant="contained">Submit</Button>
-        </div>
+            <Header/>
+            <div style={containerStyle}>
+                <h1>Create Your own poll</h1>
+                <TextField id="outlined-basic" label="Option One" variant="outlined" required={true} onChange={(e) => setOptionOne(e.target.value)}/>
+                <TextField id="outlined-basic" label="Option Two" variant="outlined" required={true} onChange={(e) => setOptionTwo(e.target.value)}/>
+                <Button variant="contained" onClick={createPoll}>Submit</Button>
+            </div>
         </div>
     )
 }
