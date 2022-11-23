7b3acacb3a61d317a054305fc7878b1f5ebc53a5
drichards188 <github@cxoprotect.com>
2022-11-23 12:28:21 -0700

    testing that user questions count increases after saved question passes

--- src/features/misc/DATA.test.js
+++ src/features/misc/DATA.test.js
@@ -1,4 +1,4 @@
-import {_getQuestions, _saveQuestion, _saveQuestionAnswer} from "./DATA";
+import {_getQuestions, _saveQuestion, _saveQuestionAnswer, _getUsers} from "./DATA";
 
 describe('_saveQuestion', () => {
     it('will return question object if successful', async () => {
@@ -18,6 +18,25 @@ describe('_saveQuestion', () => {
     it('wil return an error for an invalid request', async () => {
         let resp = _saveQuestion({author: 'sarahedo'});
         await expect(resp).rejects.toEqual('Please provide optionOneText, optionTwoText, and author');
+    })
+
+    it('will update user questions if successful', async () => {
+
+        let users = await _getUsers()
+        let originalCurrentUserQuestionCount = users['sarahedo'].questions.length;
+
+        const questionData = {
+            author: 'sarahedo',
+            optionOneText: 'pass',
+            optionTwoText: 'fail'
+        }
+        let resp = await _saveQuestion(questionData);
+        expect(resp).toBeDefined();
+
+        users = await _getUsers();
+        let newCurrentUserQuestionCount = users['sarahedo'].questions.length;
+
+        expect(originalCurrentUserQuestionCount).toBeLessThan(newCurrentUserQuestionCount);
     })
 });
 
